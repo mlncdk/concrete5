5bd6f7776437306389edb4533db3c9e502f2e13e
Mlnc <mlnc@live.dk>
2015-07-07 23:25:38 +0200

    Test af Concrete5
    Jeg tester lige github

---
+++ application/config/generated_overrides/concrete.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * -----------------------------------------------------------------------------
+ * Generated 2015-07-07T21:34:15+02:00
+ *
+ * @item      misc.access_entity_updated
+ * @group     concrete
+ * @namespace null
+ * -----------------------------------------------------------------------------
+ */
+return array(
+    'locale' => 'da_DK',
+    'site' => 'RcDigital - Test',
+    'version_installed' => '5.7.4.2',
+    'misc' => array(
+        'access_entity_updated' => 1436297655
+    )
+);
---
+++ application/files/cache/0fea6a13c52b4d47/25368f24b045ca84/38a865804f8fdcb6/57cd99682e939275/2ddb27c5cdf0b672/745d4c64665be841/30515f5ed030b71a/39748e2975957927.php
@@ -0,0 +1,16 @@
+<?php 
+/* Cachekey: cache/stash_default/doctrine/doctrinenamespacecachekey[dc2_929b19c3a686e5700cd797596160241a_]/ */
+/* Type: array */
+
+
+
+$loaded = true;
+$expiration = 1436700170;
+
+$data = array();
+
+/* Child Type: integer */
+$data['return'] = 1;
+
+/* Child Type: integer */
+$data['createdOn'] = 1436297670;
